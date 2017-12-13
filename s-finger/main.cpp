aab088e4d9cf29ce206c35cbbf25f6b51e857faa
20161104590 <Guest@scy.local>
2017-12-13 11:05:34 +0800

    S-finger
    Student

--- s-finger/main.cpp
+++ s-finger/main.cpp
@@ -7,9 +7,56 @@
 //
 
 #include <iostream>
-
+using namespace std;
+struct Student
+{
+    char name[20];
+    char num[20];
+    int age;
+    struct Student *next;
+};
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int s=1;
+    int temp=0;
+    struct Student *p,*q,*head;
+    head=NULL;
+    cout<<" 1or2"<<endl;
+    while(cin>>s,s==1)
+    {
+        p=new Student;
+        cout<<"name"<<endl;
+        cin>>p->name;
+        cout<<"num:"<<endl;
+        cin>>p->num;
+        cout<<"age:";
+        cin>>p->age;
+        if(head==NULL)
+        {
+            head=p;
+        }
+        else
+        {
+            q->next=p;
+        }
+        q=p;
+        p->next=NULL;
+        cout<<"1or2"<<endl;
+    }
+    p=head;
+    while(p!=NULL)
+    {
+        cout<<"name:"<<p->name<<"num:"<<p->num<<"age:"<<p->num<<endl;
+        p=p->next;
+    }
+    p=head;
+    if(p!=NULL)
+    {
+        q=p->next;
+        delete p;
+        p=q;
+        while(q);
+    }
     return 0;
 }
