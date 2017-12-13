5762b45fb40fcb2eed83e80f5d3de5f233670697
20161104590 <Guest@scy.local>
2017-12-13 11:01:28 +0800

    Initial Commit

---
+++ s-finger/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  s-finger
+//
+//  Created by 客人用户 on 2017/12/13.
+//  Copyright © 2017年 客人用户. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
